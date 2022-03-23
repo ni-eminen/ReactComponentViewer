4ada9aa9f3b265ea07e0c9b8560a6af22092ae28
ni-eminen <matiasnie2@gmail.com>
2022-03-23 11:19:17 +0200

    misc

---
+++ ReactComponentViewer/app.py
@@ -0,0 +1,18 @@
+# Import the required libraries
+from tkinter import *
+import webview
+# import requests
+
+# url = 'http://www.localhost:3000/api/component'
+# myobj = {'component': '<a>link</a>'}
+# x = requests.post(url, data = myobj)
+
+# Create an instance of tkinter frame or window
+win = Tk()
+
+# Set the size of the window
+win.geometry("700x350")
+
+# Create a GUI window to view the HTML content
+webview.create_window('build', 'build/index.html')
+webview.start()
