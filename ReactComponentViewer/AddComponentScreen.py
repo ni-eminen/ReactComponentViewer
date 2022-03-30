e473d67d3cf1084744c1aca83f7b9737747732ec
ni-eminen <matiasnie2@gmail.com>
2022-03-30 12:49:10 +0300

    sidebar

--- ReactComponentViewer/AddComponentScreen.py
+++ ReactComponentViewer/AddComponentScreen.py
@@ -1,11 +1,27 @@
 import tkinter as tk
+from tkinter import Button, Frame, Canvas, Widget
 
 class AddComponentScreen(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        label = tk.Label(self, text="Add Component Screen", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Go to the Login page",
-                           command=lambda: controller.show_frame("LoginScreen"))
-        button.pack()
+
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        mainframe = Frame(self,bg='blue')
+        mainframe.grid(row=0, column=0, sticky='nsew')
+
+        # Make the buttons with the icons to be shown
+        # home_b = Button(frame,bg='orange',relief='flat')
+        # set_b = Button(frame,bg='orange',relief='flat')
+        # ring_b = Button(frame,bg='orange',relief='flat')
+
+        # Put them on the frame
+        # home_b.grid(row=0,column=0,pady=10)
+        # set_b.grid(row=1,column=0,pady=50)
+        # ring_b.grid(row=2,column=0)
+
+        # Bind to the frame, if entered or left
+        # frame.bind('<Enter>',lambda e: expand())
+        # frame.bind('<Leave>',lambda e: contract())
--- ReactComponentViewer/app.py
+++ ReactComponentViewer/app.py
@@ -1,6 +1,6 @@
 # Import the required libraries
 import tkinter as tk
-from tkinter import font as tkfont  # python 3
+from tkinter import Frame, font as tkfont  # python 3
 from LoginScreen import LoginScreen
 from ComponentsScreen import ComponentsScreen
 from AddComponentScreen import AddComponentScreen
@@ -8,7 +8,7 @@ import webview
 import requests
 
 url = 'http://www.localhost:3000/api/component'
-myobj = '{"component": "const [num, setNum] = useState(0); return ( <div> <p>{ num }</p> <button onClick={() => setNum(num + 2)}>click me</button> </div> );"}'
+myobj = '{"component": "const [num, setNum] = useState(0); return ( <div> <p>{ num }</p> <button onClick={() => setNum(num + 50)}>click me</button> </div> );"}'
 headers = {'content-type': 'application/json'}
 # react_build = requests.post(url, data = myobj, headers=headers)
 
@@ -17,7 +17,7 @@ headers = {'content-type': 'application/json'}
 # f.close()
 
 
-# Create a GUI window to view the HTML content
+# # Create a GUI window to view the HTML content
 # webview.create_window('build', "react_build.html")
 # webview.start()
 
@@ -36,15 +36,24 @@ class ReactComponentViewer(tk.Tk):
         # the container is where we'll stack a bunch of frames
         # on top of each other, then the one we want visible
         # will be raised above the others
-        container = tk.Frame(self)
-        container.grid(row=0, column=0)
+        container = Frame(self)
+        container.grid(row=0, column=0, sticky='nsew')
         container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=2)
+        container.grid_columnconfigure(1, weight=10)
+        
+        sidebar = Frame(container,bg='red')
+        sidebar.grid(row=0,column=0, sticky='nsew')
+
+        content_frame = tk.Frame(container)
+        content_frame.grid(row=0, column=1, sticky='nsew')
+        content_frame.grid_rowconfigure(0, weight=1)
+        content_frame.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
         for F in (LoginScreen, AddComponentScreen, ComponentsScreen):
             page_name = F.__name__
-            frame = F(parent=container, controller=self)
+            frame = F(parent=content_frame, controller=self)
             self.frames[page_name] = frame
 
             # put all of the pages in the same location;
@@ -52,7 +61,7 @@ class ReactComponentViewer(tk.Tk):
             # will be the one that is visible.
             frame.grid(row=0, column=0, sticky="nsew")
 
-        self.show_frame("LoginScreen")
+        self.show_frame("AddComponentScreen")
 
     def show_frame(self, page_name):
         '''Show a frame for the given page name'''
