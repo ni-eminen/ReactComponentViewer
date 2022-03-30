85fb63d731e57cf22bb95287216e323ceea5c9f9
ni-eminen <matiasnie2@gmail.com>
2022-03-30 15:47:37 +0300

    text editor

--- ReactComponentViewer/AddComponentScreen.py
+++ ReactComponentViewer/AddComponentScreen.py
@@ -1,5 +1,12 @@
 import tkinter as tk
-from tkinter import Button, Frame, Canvas, Widget
+from tkinter import END, Button, Frame, Canvas, Widget, Text
+import webview
+import requests
+import idlelib.colorizer as ic
+import idlelib.percolator as ip
+import re
+import pygments
+import pyro
 
 class AddComponentScreen(tk.Frame):
     def __init__(self, parent, controller):
@@ -9,8 +16,38 @@ class AddComponentScreen(tk.Frame):
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
 
-        mainframe = Frame(self,bg='blue')
+        mainframe = Frame(self,bg='red')
         mainframe.grid(row=0, column=0, sticky='nsew')
+        mainframe.grid_rowconfigure(0, weight=1)
+        mainframe.grid_columnconfigure(0, weight=1)
+        mainframe.grid_rowconfigure(1, weight=1)
+
+        text_frame = Frame(mainframe, bg='black')
+        text_frame.grid(column=0, row=0, sticky='news')
+
+        text_editor = Text(text_frame)
+        text_editor.pack(side='top', fill='both', expand=True)
+
+        cdg = ic.ColorDelegator()
+        cdg.prog = re.compile(r'\b(?P<MYGROUP>tkinter)\b|' + ic.make_pat(), re.S)
+        cdg.idprog = re.compile(r'\s+(\w+)', re.S)
+
+        cdg.tagdefs['MYGROUP'] = {'foreground': '#7F7F7F', 'background': '#FFFFFF'}
+
+        # These five lines are optional. If omitted, default colours are used.
+        cdg.tagdefs['COMMENT'] = {'foreground': '#FF0000', 'background': '#FFFFFF'}
+        cdg.tagdefs['KEYWORD'] = {'foreground': '#007F00', 'background': '#FFFFFF'}
+        cdg.tagdefs['BUILTIN'] = {'foreground': '#7F7F00', 'background': '#FFFFFF'}
+        cdg.tagdefs['STRING'] = {'foreground': '#7F3F00', 'background': '#FFFFFF'}
+        cdg.tagdefs['DEFINITION'] = {'foreground': '#007F7F', 'background': '#FFFFFF'}
+        ip.Percolator(text_editor).insertfilter(cdg)
+
+        button_frame = Frame(mainframe, bg='red')
+        button_frame.grid(column=0, row=1, sticky='nswe')
+
+        render_button = Button(button_frame, command=lambda: render_component(text_editor.get('1.0', END)), text='Render component')
+        render_button.grid(column=0, row=0, sticky='e')
+
 
         # Make the buttons with the icons to be shown
         # home_b = Button(frame,bg='orange',relief='flat')
@@ -25,3 +62,32 @@ class AddComponentScreen(tk.Frame):
         # Bind to the frame, if entered or left
         # frame.bind('<Enter>',lambda e: expand())
         # frame.bind('<Leave>',lambda e: contract())
+
+        def render_component(component):
+            print(component)
+            url = 'http://www.localhost:3000/api/component'
+            string_json = '{{"component": "{c}"}}'.format(c=component).replace('\n', '')
+            headers = {'content-type': 'application/json'}
+
+            
+
+            print()
+            print()
+            print()
+            print(string_json)
+            print()
+            
+            print()
+            print()
+            print()
+
+            react_build = requests.post(url, data = string_json, headers=headers)
+
+            f = open("react_build.html", "w")
+            f.write(react_build.text)
+            f.close()
+
+
+            # Create a GUI window to view the HTML content
+            webview.create_window('build', "react_build.html")
+            webview.start()
--- ReactComponentViewer/app.py
+++ ReactComponentViewer/app.py
@@ -1,15 +1,16 @@
 # Import the required libraries
 import tkinter as tk
-from tkinter import Frame, font as tkfont  # python 3
+from tkinter import Frame, font as tkfont, Button
+from turtle import width  # python 3
 from LoginScreen import LoginScreen
 from ComponentsScreen import ComponentsScreen
 from AddComponentScreen import AddComponentScreen
 import webview
 import requests
 
-url = 'http://www.localhost:3000/api/component'
-myobj = '{"component": "const [num, setNum] = useState(0); return ( <div> <p>{ num }</p> <button onClick={() => setNum(num + 50)}>click me</button> </div> );"}'
-headers = {'content-type': 'application/json'}
+# url = 'http://www.localhost:3000/api/component'
+# myobj = '{"component": "const [num, setNum] = useState(0); return ( <div> <p>{ num }</p> <button onClick={() => setNum(num + 50)}>click me</button> </div> );"}'
+# headers = {'content-type': 'application/json'}
 # react_build = requests.post(url, data = myobj, headers=headers)
 
 # f = open("react_build.html", "w")
@@ -36,14 +37,21 @@ class ReactComponentViewer(tk.Tk):
         # the container is where we'll stack a bunch of frames
         # on top of each other, then the one we want visible
         # will be raised above the others
-        container = Frame(self)
+        container = Frame(self, width=100)
         container.grid(row=0, column=0, sticky='nsew')
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=2)
         container.grid_columnconfigure(1, weight=10)
         
-        sidebar = Frame(container,bg='red')
+        sidebar = Frame(container,bg='#89CFF0')
         sidebar.grid(row=0,column=0, sticky='nsew')
+
+        # Make the buttons with the icons to be shown
+        home_b = Button(sidebar, command=lambda: self.show_frame('ComponentsScreen'), bg='orange',relief='flat', text='Components')
+        set_b = Button(sidebar, command=lambda: self.show_frame('AddComponentScreen'),bg='orange',relief='flat', text='Add component')
+
+        home_b.pack(pady=10)
+        set_b.pack(pady=10)
 
         content_frame = tk.Frame(container)
         content_frame.grid(row=0, column=1, sticky='nsew')
@@ -61,7 +69,7 @@ class ReactComponentViewer(tk.Tk):
             # will be the one that is visible.
             frame.grid(row=0, column=0, sticky="nsew")
 
-        self.show_frame("AddComponentScreen")
+        self.show_frame("LoginScreen")
 
     def show_frame(self, page_name):
         '''Show a frame for the given page name'''
